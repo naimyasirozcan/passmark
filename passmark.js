619c9fa5bbbe6a997a682fdb52eedecc64492741
nyo <naimyasirozcan@gmail.com>
2022-05-24 00:50:03 +0300

    first commit

---
+++ passmark.js
@@ -0,0 +1,41 @@
+// definitions
+
+let mid = document.querySelector("#midtermInp") // text input for midterm
+let final = document.querySelector("#finalInp") // text input for final
+let result = document.querySelector('#result')
+let submitBtn = document.querySelector('#submitBtn')
+
+// function
+
+submitBtn.addEventListener('click', function(){
+    let average = (mid.value * 0.4) + (final.value * 0.6)
+    if (average >= 0 && average < 50) {
+        result.textContent = `Average: ${average} | FF - Failed`
+    }
+    else if (average >= 50 && average < 60) {
+        result.textContent = `Average: ${average} | FD - Failed`
+    }
+    else if (average >= 60 && average < 65) {
+        result.textContent = `Average: ${average} | DD - Conditional Pass`
+    }
+    else if (average >= 65 && average < 70) {
+        result.textContent = `Average: ${average} | DC - Conditional Pass`
+    }
+    else if (average >= 70 && average < 75) {
+        result.textContent = `Average: ${average} | CC - Passed!`
+    }
+    else if (average >= 75 && average < 80) {
+        result.textContent = `Average: ${average} | BC - Passed!`
+    }
+    else if (average >= 80 && average < 85) {
+        result.textContent = `Average: ${average} | BB - Passed!`
+    }
+    else if (average >= 85 && average < 90) {
+        result.textContent = `Average: ${average} | BA - Passed!`
+    }
+    else if (average >= 90 && average < 100) {
+        result.textContent = `Average: ${average} | AA - Passed!`
+    }
+    mid.value = ""
+    final.value = ""
+})
---
+++ readMe.md
@@ -0,0 +1,19 @@
+# Pass Mark Calculator
+
+This project was actually a basic codepen assignment given from patika.dev for practice on if/if else conditional loops but I decided to step it up and create a calculator that i would want to use.
+
+This simple page has been a challenge for me because I tried to write whole codes without any help or Google search, just by my knowledge. When I thought I finished it, I saw it doesn't work as it should be, then I asked my first question on Stack Overflow. After 3.5 hours crisis and researchs about input values a guy came from heaven and rescued me by saying me just replace a one row.
+
+[Stack Overflow Question and Solution](https://stackoverflow.com/questions/72354545/there-is-a-problem-when-i-try-to-use-input-value-for-taking-average-in-this-proj)
+
+***
+
+<img src="https://images2.imgbox.com/d9/82/1Bp2g5Ff_o.png" alt="passMark2" width="860"/>
+
+***
+
+<img src="https://images2.imgbox.com/5b/3b/MPhk930U_o.png" alt="passMark1" width="860"/>
+
+***
+
+### Thanks!
